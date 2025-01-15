491d854a15172b893856e73dda642d8e861cd974
Student <student@abv.bg>
2025-01-15 09:38:02 +0200

    Add project files.

---
+++ MyRecipes01/Data/Models/Recipe.cs
@@ -0,0 +1,21 @@
+﻿namespace MyRecipes01.Data.Models
+{
+    public class Recipe
+    {
+        public Recipe()
+        {
+            this.Images = new HashSet<Image>();
+            this.Ingredients = new HashSet<RecipeIngredient>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan PreparationTime { get; set; }
+        public TimeSpan CookingTime { get; set; }
+        public int PortionCount { get; set; }
+        public string Instruction { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual ICollection <Image> Images { get; set; }
+        public virtual ICollection <RecipeIngredient> Ingredients { get; set; }
+    }
+}
